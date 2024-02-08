0f021e291702243f7e3455bb297aaf9dc0c2ae5b
Bckarnati2018 <44175912+Bckarnati2018@users.noreply.github.com>
2024-02-07 21:07:33 -0500

    Update main.tf source bath for the kms resource block

--- terraform-modules/aws/eks/main.tf
+++ terraform-modules/aws/eks/main.tf
@@ -32,7 +32,7 @@ resource "aws_kms_key" "eks" {
 }
 
 module "kms_cloudwatch_log_group" {
-  source                  = "github.com/ManagedKube/kubernetes-ops.git//terraform-modules/aws/kms/cloudwatch_log_group?ref=v2.0.37"
+  source                  = "github.com/Bckarnati2018/kubernetes-ops.git//terraform-modules/aws/kms/cloudwatch_log_group"
   log_group_name          = "/aws/eks/${var.cluster_name}/cluster"
   tags                    = var.tags
 }
